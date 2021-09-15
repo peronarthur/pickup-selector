6b0ce5ae34bb37118ba0c95d8b0408f2f49964b5
gabrielHosino <gabriel.hosino@vtex.com.br>
2021-09-15 09:51:32 -0300

    Add messages

---
+++ react/utils/messages.ts
@@ -0,0 +1,15 @@
+import { defineMessages } from 'react-intl'
+
+export const editor = defineMessages({
+  pickupSelector: { title: 'admin/editor.pickup-selector.title' },
+  zipcodeInput: { title: 'admin/editor.pickup-selector.zipcode-input.title' },
+  skuSelector: { title: 'admin/editor.pickup-selector.sku-selector.title' },
+})
+
+export const zipcodeInput = defineMessages({
+  label: 'pickup-selector.zipcode-input.label',
+})
+
+export const skuSelector = defineMessages({
+  label: 'pickup-selector.sku-selector.label',
+})
