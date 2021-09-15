7727ec11c31c8d62b124a0f93e0ce6dca9d2b674
gabrielHosino <gabriel.hosino@vtex.com.br>
2021-09-15 09:53:32 -0300

    Add pickup selector container

---
+++ react/PickupSelector.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useCssHandles } from 'vtex.css-handles'
+
+import ShippingProvider from './context/shippingProvider'
+
+const CSS_HANDLES = ['pickupSelectorContainer'] as const
+
+const PickupSelector: StorefrontFunctionComponent = ({ children }) => {
+  const handles = useCssHandles(CSS_HANDLES)
+
+  return (
+    <ShippingProvider>
+      <div
+        className={`${handles.pickupSelectorContainer} flex-s justify-between-s`}
+      >
+        {React.Children.map(children, (child) => child)}
+      </div>
+    </ShippingProvider>
+  )
+}
+
+PickupSelector.schema = {
+  title: 'PickupSelector',
+  type: 'object',
+}
+
+export default PickupSelector
