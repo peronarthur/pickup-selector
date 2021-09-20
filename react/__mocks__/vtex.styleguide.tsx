b57fd126d3972a721afec654032af8b263ad5763
gabrielHosino <gabriel.hosino@vtex.com.br>
2021-09-20 17:47:26 -0300

    Add tests config

---
+++ react/__mocks__/vtex.styleguide.tsx
@@ -0,0 +1,2 @@
+export { default as Card } from '@vtex/styleguide/lib/Card'
+export { default as Input } from '@vtex/styleguide/lib/Input'
---
+++ react/typings/@vtex/styleguide.d.ts
@@ -0,0 +1,2 @@
+declare module '@vtex/styleguide/lib/Card'
+declare module '@vtex/styleguide/lib/Input'
