454fe4062ac6953c6696c74bc5a3d61d30538597
gabrielHosino <gabriel.hosino@vtex.com.br>
2021-09-21 09:35:40 -0300

    Add new test to pickupSlaList component

--- react/__tests__/PickupSlaList.test.tsx
+++ react/__tests__/PickupSlaList.test.tsx
@@ -1,10 +1,20 @@
+/* eslint-disable react/no-children-prop */
 import React from 'react'
 import { render } from '@vtex/test-tools/react'
 
 import PickupSlaList from '../PickupSlaList'
 import ShippingContext from '../context/shippingContext'
+import OptionCard from '../OptionCard'
 // eslint-disable-next-line jest/no-mocks-import
 import contextValuesMock from '../__mocks__/context'
+
+jest.mock('../OptionCard', () => {
+  return {
+    __esModule: true,
+    // eslint-disable-next-line react/display-name
+    default: () => <div>hi</div>,
+  }
+})
 
 describe('PickupSlaList', () => {
   const slaResponseListMock = {
@@ -46,7 +56,11 @@ describe('PickupSlaList', () => {
     ],
   }
 
-  it('Should render the list cards', () => {
+  const children = [
+    <OptionCard key="1" cardInfo={slaResponseListMock.pickupSlas[0]} />,
+  ]
+
+  it('Should render the list', () => {
     const { getByTestId } = render(
       <ShippingContext.Provider
         value={{ ...contextValuesMock, ...slaResponseListMock }}
@@ -58,5 +72,19 @@ describe('PickupSlaList', () => {
     const slaList = getByTestId('card-list')
 
     expect(slaList).toBeTruthy()
+  })
+
+  it('Should render the correct amount of cards', () => {
+    const { getByTestId } = render(
+      <ShippingContext.Provider
+        value={{ ...contextValuesMock, ...slaResponseListMock }}
+      >
+        <PickupSlaList children={children} />
+      </ShippingContext.Provider>
+    )
+
+    const slaList = getByTestId('card-list')
+
+    expect(slaList.children).toHaveLength(2)
   })
 })
