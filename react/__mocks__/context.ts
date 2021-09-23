e4a3e3447f750101d3a06ecf28cd9f3488dd908a
gabrielHosino <gabriel.hosino@vtex.com.br>
2021-09-23 09:31:17 -0300

    Add pickupPointAddress block

--- react/__mocks__/context.ts
+++ react/__mocks__/context.ts
@@ -2,7 +2,31 @@ const contextValuesMock = {
   zipcode: '',
   selectedItem: {},
   selectedQuantity: '0',
-  pickupSlas: [],
+  pickupSlas: [
+    {
+      id: 'store id',
+      address: {
+        street: 'street',
+        number: '3',
+        city: 'city',
+        state: 'uf',
+        neighborhood: 'neighborhood',
+      },
+      deliveryChannel: '',
+      price: 1000,
+      estimate: '2bd',
+      isSelected: false,
+      friendlyName: 'Store name',
+      businessHours: [
+        {
+          dayNumber: '1',
+          closed: false,
+          closingTime: '18',
+          openingTime: '8',
+        },
+      ],
+    },
+  ],
   setZipcode: () => {},
   setPickupSlas: () => {},
 }
---
+++ react/__tests__/PickupPointAddress.test.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { render } from '@vtex/test-tools/react'
+
+import PickupPointAddress from '../PickupPointAddress'
+import ShippingContext from '../context/shippingContext'
+import CardContext from '../context/CardContext'
+// eslint-disable-next-line jest/no-mocks-import
+import contextValuesMock from '../__mocks__/context'
+
+describe('PickupPointAddress', () => {
+  const index = 0
+  const { address } = contextValuesMock.pickupSlas[index]
+
+  const { street, city, state, number, neighborhood } = address
+  const line1 = `${street} ${number}`
+  const line2 = `${neighborhood}`
+  const line3 = `${city} - ${state}`
+
+  it('Should show the store address', () => {
+    const { queryByText } = render(
+      <ShippingContext.Provider value={contextValuesMock}>
+        <CardContext.Provider value={{ index }}>
+          <PickupPointAddress address={address} />
+        </CardContext.Provider>
+      </ShippingContext.Provider>
+    )
+
+    expect(queryByText(line1)).toBeInTheDocument()
+    expect(queryByText(line2)).toBeInTheDocument()
+    expect(queryByText(line3)).toBeInTheDocument()
+  })
+})
